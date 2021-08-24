aea92e55ee484700425a1160aae2878bc066e4b3
Matze <maze.lbr@gmail.com>
2021-08-24 18:08:50 +0200

    fixed some labels

--- plot_additional_lyapunov.py
+++ plot_additional_lyapunov.py
@@ -9,7 +9,6 @@ fig, axs = plt.subplots(2,1, figsize=(12,7),sharex=True)
 axs[0].plot(r_adv, le_adv[4,:], label="Lyapunov Exponent", linewidth=1)
 for i in range(1,50):
     axs[1].scatter(data_os[:, 0], data_os[:,i], linewidths=0, s=0.2, alpha=0.4, c='red')
-axs[0].legend(["Bifurkationsdiagram"])
 
 axs[0].grid()
 axs[1].grid()
@@ -28,7 +27,7 @@ index = data[:,0]
 header = np.loadtxt("differences.csv", delimiter=",", max_rows=1)
 rs = header[2:]
 fig, axes = plt.subplots(ncols=2,nrows=3, sharex=True, figsize=(10,7))
-fig.suptitle(r"Entwicklung der Trajektorien and Abweichungen for $x_0=$" + f"{header[0]} und "+r"$\delta =$"+f"{header[1]}", fontsize=15)
+fig.suptitle(r"Entwicklung der Trajektorien and Differenzen $\Delta x_n$ für $x_0=$" + f"{header[0]} und "+r"$\delta =$"+f"{header[1]}", fontsize=15)
 for i in range(0,3):
     axes.flat[2*i].plot(index, data[:,3*i+1], linewidth=1,linestyle = "solid")
     axes.flat[2*i].plot(index, data[:,3*i+2], linewidth=1,linestyle = "dashed")
@@ -39,9 +38,10 @@ for i in range(0,3):
     axes.flat[2*i+1].legend([r"$\Delta x_n$"],loc='lower right')
     #title
     axes.flat[2*i].set_title(r"Trajektorien für $r=$"+ f"{rs[i]}")
-    axes.flat[2*i+1].set_title(r"Abweichung für $r=$"+ f"{rs[i]}")
-    #axes
-    axes.flat[2*i].set_ylabel(r"$x_n$")
+    axes.flat[2*i+1].set_title(r"$\Delta x_n$ für $r=$"+ f"{rs[i]}")
+    
+    
+    
     
     if i == 2:
         axes.flat[2*i].set_xlabel(r"Zeitschritt $n$")
