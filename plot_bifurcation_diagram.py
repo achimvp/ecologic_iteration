656033a1de46cdb9f11c5fa80d7ad316881c0789
a.vonprittwitzundg <a.vonprittwitzundg@stud.uni-goettingen.de>
2021-08-26 14:26:00 +0300

    x-Achse hochgeschoben in zweistufigem Bifurkationsdiagramm

--- plot_bifurcation_diagram.py
+++ plot_bifurcation_diagram.py
@@ -12,7 +12,7 @@ for i in range(1,50):
     axs[0].scatter(data_ts[:, 0], data_ts[:,i], linewidths=0, s=0.2, alpha=0.1, c='blue')
     axs[1].scatter(data_os[:, 0], data_os[:,i], linewidths=0, s=0.2, alpha=0.1, c='red')
 # Styling of the plot
-axs[0].set_ylim(ymax = 10, ymin = 0)
+axs[0].set_ylim(ymax = 10, ymin = -0.5)
 axs[0].set_ylabel(r"$x_{\infty}$")
 axs[1].set_ylabel(r"$x_{\infty}$")
 axs[0].set_xlabel(r"Bifurkationsparameter $r$")
@@ -21,5 +21,5 @@ axs[0].set_title("Zweistufige Iteration")
 axs[1].set_title("Einstufige Iteration")
 plt.tight_layout()
 #export
-plt.savefig("img/ecologic_map_bifurcation diagrams")
+plt.savefig("img/ecologic_map_bifurcation_diagrams")
 #plt.show()
